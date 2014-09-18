2ecda304b384062b1a14903307ef7cb9a6e56b50
lw2533315 <lwan1@student.rcc.edu>
2014-09-19 01:26:03 +0800

    Update assignment2.s

--- homwk/assignment2.s
+++ homwk/assignment2.s
@@ -1,32 +1,32 @@
  .global _startf
 _start:
-mov r2, #102
-mov r3, #7
-cmp r2,r3
-beq aeb
-blt asb
-mov r0,#0
+   mov r2, #102
+   mov r3, #7
+   cmp r2,r3
+   beq aeb
+   blt asb
+   mov r0,#0
 
 ab:
-subs r2, r2,r3
+   subs r2, r2,r3
 
-add r0, r0,#1
-cmp r3,r2
-blt ab
-mov r1,r2
-b end
+   add r0, r0,#1
+   cmp r3,r2
+   blt ab
+   mov r1,r2
+   b end
 
 
 asb:
-mov r0,#0
-mov r1,#102
-b end
+   mov r0,#0
+   mov r1,#102
+   b end
 
 
 aeb:
-mov r0,#1
-mov r1,#0
-b end
+   mov r0,#1
+   mov r1,#0
+   b end
 
 end:
-swi 0
+   swi 0
