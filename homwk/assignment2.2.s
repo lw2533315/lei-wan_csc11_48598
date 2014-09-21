67ac267957d3a7a2f2c4ca98c51f1c261716f0f2
lw2533315 <lwan1@student.rcc.edu>
2014-09-21 23:21:13 +0800

    2

---
+++ homwk/assignment2.2.s
@@ -0,0 +1,30 @@
+  .global _start
+ _start:
+      mov r2, #102
+	  mov r3,#7
+	  mov r4,#0
+	  cmp r2,r3
+	  beq equal
+	  blt small
+
+ab:
+      subs r2, r2,r3
+	  adds r4 r4, #1
+	  cmp r2,r3
+	  blt small
+	  bge ab
+
+
+small:
+      mov r1,r4
+	  mov r0,r2
+	  b end
+
+equal:
+      mov r1,r4
+	  mov r0,#0
+	  b end
+
+end:
+     mov r7,#1
+	 swi 0
