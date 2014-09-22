2d0ee7b16fed802d4dfae768fd14938ffb545f35
lw2533315 <lwan1@student.rcc.edu>
2014-09-22 11:52:39 +0800

    Update assignment2.s

--- homwk/assignment2.s
+++ homwk/assignment2.s
@@ -2,31 +2,32 @@
 _start:
    mov r2, #102
    mov r3, #7
+   mov r4,#0
    cmp r2,r3
-   beq aeb
-   blt asb
-   mov r0,#0
+   beq equal
+   blt small
+   
 
 ab:
    subs r2, r2,r3
 
-   add r0, r0,#1
-   cmp r3,r2
-   blt ab
+   add r4, r4,#1
+   cmp r2,r3
+   blt small
+   bge ab
+  
+
+small:
+   mov r0,r4
    mov r1,r2
    b end
 
 
-asb:
-   mov r0,#0
-   mov r1,#102
-   b end
-
-
-aeb:
-   mov r0,#1
+equal:
+   mov r0,r4
    mov r1,#0
    b end
 
 end:
+   mov r7,#1
    swi 0
