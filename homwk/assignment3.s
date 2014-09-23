cfe2734d643221dfdcfbf40cf41f228dc422e440
lw2533315 <lwan1@student.rcc.edu>
2014-09-23 21:15:40 +0800

    assignment3

--- homwk/assignment3.s
+++ homwk/assignment3.s
@@ -6,38 +6,41 @@ main:
 	mov r3, #5       /* b=5 */
 	mov r0, #0       /* r0=0 -> counter */
 	mov r1, r2       /* r1=a=111 */
-	mov r4, #0   
+	mov r4, #0 
+        mov r8, #10  
 	
 	
 	cmp  r1, r3      /* compare and leave if less */
 	blt  swap
 	/* Now just perform repeated subtractions */
-repeat_subtraction:
-    mov r6, #1       @record how many times
-	mov r8, #10
-	mov r7, #0       @ r3*r6
-	mov r9, #0
-	mul r7, r6,r3
-	mul r9, r7,r8
-	cmp r1,r9
-	blt small
-	
-repeat_scale:
-    mul r6, r6,r8     
-    mul r7, r6,r3
-    mul r9, r7,r8
-    
-    cmp r1,r9
-    
-    bge repeat_scale
+repeat2:
+       mov r6,#1
+       mul r7,r6,r3
+       mul r9, r7,r8
+       cmp r1,r9
+       blt small
+repeat:
+       mul r6, r6,r8
+       mul r7, r6, r3
+       mul r9, r7, r8
+       cmp r1,r9
+       bge repeat
+       add r0, r0, r6
+       sub r1, r1, r7
+       cmp r1, r3
+       bge repeat2  
+       b   end 
+
 small:
-    sub r1,r1,r7 
-    add r0, r0, r6  
-    
-    cmp r1, r7 
-    
-	
-	bge repeat_subtraction    
+       sub r1, r1, r3
+       add r0, r0, #1
+       cmp r1, r3
+       bge small
+       b   end
+
+
+
+
 swap:
 	/* Exchange r0 and r1 using r5 with a swap if flag set */
 	cmp r4, #0
@@ -47,4 +50,3 @@ swap:
 	mov r1, r5
 end:
 	bx lr
-	
