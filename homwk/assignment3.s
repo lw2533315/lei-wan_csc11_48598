2597a81c6f3923cec3cdc6562a1baf45f95f7f6e
lw2533315 <lwan1@student.rcc.edu>
2014-09-24 13:21:59 +0800

    Update assignment3.s

--- homwk/assignment3.s
+++ homwk/assignment3.s
@@ -2,11 +2,11 @@
 .global _start
 _start:
 	/* Initialize */
-	mov r1, #255   
+	mov r1, #255      @mov the maxium is 255
 	mov r3, #5       /* b=5 */
 	mov r0, #0       /* r0=0 -> counter */
-	mov r2, #0
-	mov r7, #0
+	mov r2, #0       @除数的倍数
+	mov r7, #0       
 	mov r9,#0
 	mov r4, #0 
         mov r8, #10  
@@ -25,7 +25,7 @@ repeat:
        mul r2, r6,r8
        mul r7, r2, r3
        mul r9, r7, r8
-       cmp r1,r9
+       cmp r1,r9             
        bge repeat
        add r0, r0, r2
        sub r1, r1, r7
