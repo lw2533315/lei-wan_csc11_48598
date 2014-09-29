de3c6a0c980e929bfe1b0b188a016647f9fefd2a
lw2533315 <lwan1@student.rcc.edu>
2014-09-29 12:18:19 -0700

    Update assignment2.2.s

--- homwk/assignment2.2.s
+++ homwk/assignment2.2.s
@@ -1,30 +1,28 @@
   .global _start
- _start:
-      mov r2, #102
-	  mov r3,#7
-	  mov r4,#0
-	  cmp r2,r3
-	  beq equal
-	  blt small
-
-ab:
-      subs r2, r2,r3
-	  adds r4, r4, #1
-	  cmp r2,r3
-	  blt small
-	  bge ab
-
-
-small:
-      mov r1,r4
-	  mov r0,r2
-	  b end
-
-equal:
-      mov r1,r4
-	  mov r0,#0
-	  b end
-
-end:
-     mov r7,#1
-	 swi 0
+  _start:
+  mov r2,#116
+  mov r3, #5
+  mov r0, #0
+  mov r1, r2
+  mov r4, #0
+  cmp r1,r3
+  blt swap
+  
+  
+  repeat:
+  add r0,r0,#1
+  sub r1,r1,r3
+  cmp r1,r3
+  bge repeat
+  
+  swap:
+  cmp r4,#0
+  beq end;
+  mov r5,r0
+  mov r0,r1
+  mov r1,r5
+  
+  
+  end:
+ mov r7,#1
+ swi 0
