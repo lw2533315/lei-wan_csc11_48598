657ec40109716a7748e68772a37418f92a040424
lw2533315 <lwan1@student.rcc.edu>
2014-10-01 11:34:17 +0800

    Update lsl.s

--- homwk/lsl.s
+++ homwk/lsl.s
@@ -3,8 +3,8 @@ _start:
 
 mov r0, #111
 mov r1, #5
-mov r5, #5
-mov r4, #2
+mov r5, #0
+mov r4, #2    @times of shift
 mov r2, #0
 mov r3, #0
 
@@ -15,9 +15,9 @@ blt swap
 
 loop:
 mov r6,#1
-mov r7,#1
+mov r7,#1    @couter 
 mov r5,r1
-repeat:
+repeat:      @shift test
 mov r6,r7
 mov r5, r5,lsl#1
 cmp r0,r5
@@ -26,7 +26,7 @@ mul r7,r6,r4
 bge repeat
 
 small:
-mov r5, r5,lsr#1
+mov r5, r5,lsr#1    @return 1 shift
 sub r0,r0,r5
 add r2,r2,r7
 cmp r0,r1
