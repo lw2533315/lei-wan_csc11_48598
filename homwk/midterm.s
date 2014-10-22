bcfc0aab0d349c1f4841db48c6eb9fcf6b278e6d
lw2533315 <lwan1@student.rcc.edu>
2014-10-22 11:44:44 +0800

    Update midterm.s

--- homwk/midterm.s
+++ homwk/midterm.s
@@ -48,6 +48,8 @@ int9: .asciz"Monthly charge is %d\n"
 int10: .asciz"Enter the term on the sequence\n"
 
 
+
+
 .balign 4
 cmp1: .asciz"a"
 
@@ -295,27 +297,7 @@ end5:
 pop {lr}
 bx lr
 
-fibon:
-push {lr}
-mov r2,#0
-cmp r0,#0
-beq firun
-cmp r0,#1
-bet firun1
-sub r0,r0,#1
-cmp r0,#1
-mov r2,#
-bl fibon
 
-firun:
-add r2,r2,#0
-bl fiend
-
-firun1:
-add r2,r2,#1
-bl fiend
-
-fiend: 
 
 
 fibonacci:
@@ -328,12 +310,46 @@ ldr r1,address_input1
 bl scanf
 
 ldr r0, address_input1
-ldr r1,[r1]
-bl fibon                                        @not finish
+ldr r0,[r0]
+mov r2,#0
+cmp r0,#0
+beq firun
+cmp r0,#1
+beq firun1
+cmp r0,#3
+bge firun2
+mov r3,#0
+mov r4,#1
 
+
+
+
+firun2:
+add r2,r3,r4
+mov r3,r4
+mov r4,r2
+sub r0,r0,#1
+cmp r0,#3
+bge firun2
+b fiend
+
+
+firun:
+add r2,r2,#0
+b fiend
+
+firun1:
+add r2,r2,#1
+b fiend
+
+fiend: 
+mov r3,r0
+ldr r0, address_pattern2
+ldr r1,r3
+bl printf
 
 pop {lr}
-
+bx lr
 
 .global main
 main:
