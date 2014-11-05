82d22263b4b3755d64a593d23730be44f9777437
lw2533315 <lwan1@student.rcc.edu>
2014-11-05 11:45:22 -0800

    Update collatz_combi.s

--- homwk/assignment6/collatz_combi.s
+++ homwk/assignment6/collatz_combi.s
@@ -13,22 +13,22 @@ collatz:
 bl time
 mov r5,r0
   collatz_repeat:
-    mov r1, r4                 /* r1 ← r0 */
-    mov r0, #0                 /* r0 ← 0 */
+    mov r1, r4                 /* r1 隆没 r0 */
+    mov r0, #0                 /* r0 隆没 0 */
   collatz_loop:
     cmp r1, #1                 /* compare r1 and 1 */
     beq collatz_end            /* if r1 == 1 branch to collatz_end */
-    and r2, r1, #1             /* r2 ← r1 & 1 */
+    and r2, r1, #1             /* r2 隆没 r1 & 1 */
     cmp r2, #0                 /* compare r2 and 0 */
     bne collatz_odd            /* if r2 != 0 (this is r1 % 2 != 0) branch to collatz_odd */
   collatz_even:
-    mov r1, r1, ASR #1         /* r1 ← r1 >> 1. This is r1 ← r1/2 */
+    mov r1, r1, ASR #1         /* r1 隆没 r1 >> 1. This is r1 隆没 r1/2 */
     b collatz_end_loop         /* branch to collatz_end_loop */
   collatz_odd:
-    add r1, r1, r1, LSL #1     /* r1 ← r1 + (r1 << 1). This is r1 ← 3*r1 */
-    add r1, r1, #1             /* r1 ← r1 + 1. */
+    add r1, r1, r1, LSL #1     /* r1 隆没 r1 + (r1 << 1). This is r1 隆没 3*r1 */
+    add r1, r1, #1             /* r1 隆没 r1 + 1. */
   collatz_end_loop:
-    add r0, r0, #1             /* r0 ← r0 + 1 */
+    add r0, r0, #1             /* r0 隆没 r0 + 1 */
     b collatz_loop             /* branch back to collatz_loop */
   collatz_end:
     sub r3, r3, #1
@@ -37,7 +37,7 @@ mov r5,r0
     
     mov r0,#0
 bl time
-mov r6,#0
+mov r6,r0
 sub r6,r6,r5
 ldr r0, ad_m1
 mov r1,r6
@@ -60,18 +60,18 @@ bl printf
 bl time
 mov r5,r0
   collatz_repeat2:
-    mov r1, r4                 /* r1 ← r0 */
-    mov r0, #0                 /* r0 ← 0 */
+    mov r1, r4                 /* r1 隆没 r0 */
+    mov r0, #0                 /* r0 隆没 0 */
   collatz2_loop:
     cmp r1, #1                 /* compare r1 and 1 */
     beq collatz2_end           /* if r1 == 1 branch to collatz2_end */
-    and r2, r1, #1             /* r2 ← r1 & 1 */
+    and r2, r1, #1             /* r2 隆没 r1 & 1 */
     cmp r2, #0                 /* compare r2 and 0 */
-    moveq r1, r1, ASR #1       /* if r2 == 0, r1 ← r1 >> 1. This is r1 ← r1/2 */
-    addne r1, r1, r1, LSL #1   /* if r2 != 0, r1 ← r1 + (r1 << 1). This is r1 ← 3*r1 */
-    addne r1, r1, #1           /* if r2 != 0, r1 ← r1 + 1. */
+    moveq r1, r1, ASR #1       /* if r2 == 0, r1 隆没 r1 >> 1. This is r1 隆没 r1/2 */
+    addne r1, r1, r1, LSL #1   /* if r2 != 0, r1 隆没 r1 + (r1 << 1). This is r1 隆没 3*r1 */
+    addne r1, r1, #1           /* if r2 != 0, r1 隆没 r1 + 1. */
   collatz2_end_loop:
-    add r0, r0, #1             /* r0 ← r0 + 1 */
+    add r0, r0, #1             /* r0 隆没 r0 + 1 */
     b collatz2_loop            /* branch back to collatz2_loop */
   collatz2_end:
     sub r3, r3, #1
