0e43ff1b9c5a9c2f97d6f1a195d0a372eb4987d3
Lei Wan <lw2533315@CSC11.(none)>
2014-12-01 19:53:47 -0800

    7c

--- homwk/assignment7c/timecompare.s
+++ homwk/assignment7c/timecompare.s
@@ -1,13 +1,15 @@
 .data
-m1:.asciz"enter the temperatrue in F int\n"
+m1:.asciz"enter the temperature in F int\n"
 m4:.asciz"enter the temperature in F float\n"
-m2:.asciz"The temperture in C is %d\n"
+m2:.asciz"The temperature in C is %d\n"
+m5:.asciz"The tremperature in C is %f\n"
 m3:.asciz"It take %d seconds\n"
 format:.asciz"%d"
 format1:.asciz"%f"
 .balign 4
 store1:.word 0
 store2:.word 0
+store3:.word 0
 f1:.float 9
 f2:.float 5
 f3:.float 32
@@ -17,7 +19,7 @@ f: .word 0
 
 .global main
 main:
-push {lr}
+push {r10,lr}
 
 
 ldr r0,ad_m1
@@ -34,7 +36,7 @@ ldr r0,ad_format1
 ldr r1,ad_store2
 bl scanf            @temperature in F
 
-ldr r6,=100000000   @loop times
+ldr r6,=10000  @loop times
 
 mov r0,#0
 bl time
@@ -48,17 +50,8 @@ ble second
 ldr r1,ad_f1
 vldr s9,[r1]
 
-ldr r0,ad_m1
-bl printf
-
-
 ldr r1,ad_f2
 vldr s10,[r1]
-
-
-ldr r0,ad_format1
-ldr r1,ad_store2
-bl scanf
 
 ldr r1,ad_store2
 vldr s11,[r1]
@@ -73,13 +66,11 @@ vdiv.f32 s11,s11,s9
 sub r6,r6,#1
 b loopshift
 
-
 second:
 
 mov r0,#0
 bl time
 mov r12,r0
-
 
 sub r12,r12,r11
 
@@ -87,12 +78,15 @@ ldr r0,ad_m3
 mov r1,r12
 bl printf          @take time
 
+
 vcvt.f64.f32 d7,s11
-ldr  r0,ad_m2
+ldr  r0,ad_m5
 vmov r2,r3,d7
 bl printf
 
-ldr r6,=100000000  @loop times
+
+
+ldr r6,=10000  @loop times
 
 mov r0,#0
 bl time
@@ -139,7 +133,7 @@ ldr r0, ad_m2
 mov r1,r4
 bl printf
 
-pop {lr}
+pop {r10,lr}
 bx lr
 ad_f:.word f
 ad_f3:.word f3
@@ -149,9 +143,11 @@ ad_m1:.word m1
 ad_m2:.word m2
 ad_m3:.word m3
 ad_m4:.word m4
+ad_store3:.word store3
 ad_format:.word format
 ad_store1:.word store1
 ad_store2:.word store2
-ad_format:.word format1
+ad_format1:.word format1
+ad_m5:.word m5
 .global printf
 .global scanf
