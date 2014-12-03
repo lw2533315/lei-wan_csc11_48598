cbbbbb3afb923a862d99eff67090abf2b6f062f9
lw2533315 <lwan1@student.rcc.edu>
2014-12-03 15:58:25 +0800

    Signed-off-by: lw2533315 <lwan1@student.rcc.edu>

--- homwk/assignment7d/divmod.s
+++ homwk/assignment7d/divmod.s
@@ -1,31 +1,30 @@
 
 .data
-
 .text
 .global scaleright
 scaleRight:
-	push {r4,lr}
+	push {lr}
 	doWhile_r1_lt_r2:
 		mov r3,r3,ASR #1
 		mov r2,r2,ASR #1
 	cmp r1,r2
 	blt doWhile_r1_lt_r2
-	pop {r4,lr}
+	pop {lr}
     bx lr
 .global addsub
 addsub:
-	push {r4,lr}
+	push {lr}
 	doWhile_r3_ge_1:
 		add r0,r0,r3
 		sub r1,r1,r2
 		bl scaleRight
 	cmp r3,#1
 	bge doWhile_r3_ge_1
-    pop {r4,lr}
+    pop {lr}
     bx lr
 .global scaleleft
 scaleleft:
-	push {r4,lr}
+	push {lr}
 	doWhile_r1_ge_r2:
 		mov r3,r3,LSL #1
 		mov r2,r2,LSL #1
@@ -33,11 +32,11 @@ scaleleft:
 	bge doWhile_r1_ge_r2
 	mov r3,r3,ASR #1
 	mov r2,r2,ASR #1
-	pop {r4,lr}
+	pop {lr}
     bx lr
 .global div
 div:
-	push {r4,lr}
+	push {lr}
 	mov r0,#0
 	mov r3,#1
 	cmp r1,r2
@@ -45,7 +44,5 @@ div:
 		bl scaleleft
 		bl addsub
 	end:
-       
-       
-	pop {r4,lr}
+	pop {lr}
     bx lr
