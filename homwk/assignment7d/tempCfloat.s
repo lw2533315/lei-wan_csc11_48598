480f0a6fdda3db79af936d5e352571d930e731cb
lw2533315 <lwan1@student.rcc.edu>
2014-12-05 12:11:38 +0800

    Signed-off-by: lw2533315 <lwan1@student.rcc.edu>

--- homwk/assignment7d/tempCfloat.s
+++ homwk/assignment7d/tempCfloat.s
@@ -1,16 +1,11 @@
-
 .data
 
-m1:.asciz"the temperature F  is %f \n"
-m2:.asciz"The C-temperatrue is %f \n"
+m1:.asciz"the temperature F  is %d \n"
+m2:.asciz"The C-temperatrue is %d \n"
 format:.asciz"%d"
-f1:.float 32.0
-f2:.float 212.0
-f3:.float 5.0
-f4:.float 9.0
 
 .balign 4
-first:.skip 400
+first:.skip 200
 .text
 
 .global main
@@ -19,25 +14,17 @@ push {r4,lr}
 
 ldr r9,ad_first
 mov r10,#0
-
-
-
-ldr r6,=f1
-
-
+mov r5,#212
+mov r6,#32
 
 loop:
 
-vstr s6, [r9,r10,lsl#2]
+str r6, [r9,r10,lsl#2]
+
 
 add r10,r10,#1
-ldr r7,=f3
-vldr s7,[r7]
-vadd.f32  s6,s6,s7
-
-
-
-cmp r10,#37
+add r6,r6,#5
+cmp r6,r5
 ble loop
 
 
@@ -49,16 +36,11 @@ ldr r9,=first
 
 
 ldr r1,[r9,r11,lsl#2]
-vldr s14,[r1]		@out in array
-vcvt.f64.f32 d5,s14
-ldr r0,=m1
-vmov r2,r3,d5
+ldr r0,=m1		@out in array
 bl printf
-
 
 ldr r1, [r9, r11, lsl#2]
 add r11, r11, #1
-/*
 ldr r2,=0x8e38f
 sub  r1,r1,#32
 mul r1,r2,r1
@@ -73,9 +55,9 @@ ldr r0,ad_m2
 mov r1,r1
 bl printf
 
-*/
+
 cmp r10,r11
-bgt loop1
+bne loop1
 
 pop {r4,lr}
 bx lr
