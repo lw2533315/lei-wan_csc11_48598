fe1704549dce3988ad5ef27ae8e55044bfdfef90
Lei Wan <lw2533315@CSC11.(none)>
2014-12-05 18:12:19 -0800

    final

--- book/final2/equation.s
+++ book/final2/equation.s
@@ -2,7 +2,7 @@
 m1:.asciz"We have a function ax^2+bx\n a=0.037;b=0.876.\n please enter the x's value(0,255)\n"
 m2:.asciz"ax^2+bx= %d \n"
 format:.asciz"%d"
-
+m3:.asciz"out of scale\n"
 .balign 4
 save:.word 0
 
@@ -13,6 +13,7 @@ push {r4,lr}
 ldr r5,=0x12b02  @a  20bit >>20
 ldr r6,=0xe04188  @b 24bit >>24
 
+loop:
 ldr r0,=m1
 bl printf
 
@@ -22,7 +23,21 @@ bl scanf
 
 ldr r1,=save
 ldr r1,[r1]              @r1 is value of x
+cmp r1,#0
+bge second
+ldr r0,=m3
+bl printf
+b loop
 
+second:
+cmp r1,#255
+ble calculate
+ldr r0,=m3
+bl printf
+b loop
+
+
+calculate:
 mul r2,r5,r1       @ 28bit  >>20   ax
 mov r2,r2,asr#4    @ 24bit  >>16   ax
 mul r2,r1,r2       @ 32bit   >>16  ax^2
